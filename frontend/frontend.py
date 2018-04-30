775cf0e38dc188d3d1d5e010e2f9f3f4655f6507
artidoro <apagnoni@college.harvard.edu>
2018-04-29 20:49:07 -0400

    beginning work on frontend

---
+++ frontend/frontend.py
@@ -0,0 +1,110 @@
+# frontend.py
+
+import socket
+import sys
+import threading
+import os
+import kafka
+
+import frontend_receive
+import frontend_data
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../networking_utils/'))
+import handle_messages
+
+# define header length, version code and op codes
+HEADER_LEN = 6
+version = b'\x01'
+
+# opcode associations; note that these opcodes will be sent by the client
+opcodes = {
+    b'\x10': server_receive.transfer,  # create_request
+    b'\x20': server_receive.login,  # login_request
+    b'\x30': server_receive.logout_request,
+    b'\x40': server_receive.transfer,  # delete_request
+    b'\x50': server_receive.transfer,  # list_request
+    # b'\x60': server_receive.retrieve_request
+    b'\x70': server_receive.transfer  # send_request
+}
+
+
+def message_handler(sock, lock, producer, frontend_id):
+    """
+    Function that listens on socket connection and handles messages.
+
+    Upon reception of a message the message header is parsed and unpacked and the message is redirected to the
+    appropriate function handler. The function checks that the message is using the correct version of the protocol.
+    We only support version 1. If the connection is down the function will close the current socket and log the user
+    out.
+
+    :param sock: socket object
+    :param lock: threading.lock
+    :return:
+    """
+    while True:
+        # Retrieve header data
+        try:
+            header, payload = handle_messages.recv_message(sock)
+        except:
+            print("A user has been disconnected.")
+            frontend_receive.logout_user(lock)
+            # TODO make sure the other thread also dies
+            sock.close()
+            return
+
+        payload_version = header[0]
+        payload_size = header[1]
+        opcode = header[2]
+
+        # Version does not match: for now, log user out!
+        if payload_version != version:
+            print("Version number did not match. The user has been disconnected.")
+            frontend_receive.logout_user(lock)
+            # TODO make sure the other thread also dies
+            sock.close()
+            return
+
+        # Try to send packet to correct handler
+        try:
+            opcodes[opcode](sock, header, payload_size, payload, producer, frontend_id, lock)
+        except KeyError:
+            print("Error while handling request. The user has been disconnected.")
+            server_receive.logout_user(lock)
+            sock.close()
+            return
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 5:
+        print("ERROR: Usage 'python3 frontend.py <port> <frontend_id> <broker_host> <broker_port>'")
+        sys.exit()
+
+    # parse arguments
+    server_port = sys.argv[1]
+    frontend_id = sys.argv[2]
+    broker_host = sys.argv[3]
+    broker_port = sys.argv[4]
+
+    frontend_data.broker_host = broker_host
+    frontend_data.broker_port = broker_port
+
+    # setup socket
+    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_sock.bind(('', int(server_port)))
+    server_sock.listen(5)	 # param represents the number of queued connections
+
+    # Setup a kafka producer
+    producer = kafka.KafkaProducer(bootstrap_servers=[broker_host + ':' + broker_port])
+    # todo is there a way to check that the connection was actually successful?
+
+    # lock that threads will acquire to access user data base in frontend_data
+    data_lock = threading.Lock()
+
+    while True:
+        # for every new connection create a new threads
+        client_sock, address = server_sock.accept()
+
+        print("Connected to a new client.")
+        # start thread handeling requests from the client, and producing to kafka
+        threading.Thread(target=message_handler, args=(client_sock, data_lock, producer, frontend_id), daemon=True).start()
+
---
+++ frontend/frontend_send.py
@@ -0,0 +1,1 @@
+# function called by the consumer thread to send the message from the backend to the client
