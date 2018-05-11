ca1ae788f341c48ac699b818198b43ddab530a1c
Henrique Vaz <henriquevgvaz@gmail.com>
2018-05-11 12:54:34 -0400

    attaching terminal to docker containers

--- start_frontend.sh
+++ start_frontend.sh
@@ -1,3 +1,3 @@
 docker build -f Dockerfile.frontend -t frontend . && \
 
-docker run --rm --net 262-network --ip='172.18.0.10' --name frontend frontend
+docker run --rm -it --net 262-network --ip='172.18.0.10' --name frontend frontend
--- start_kafka.sh
+++ start_kafka.sh
@@ -4,4 +4,4 @@ docker network rm 262-network
 
 docker network create --subnet=172.18.0.0/16 262-network && \
 
-docker run --rm -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=kafka --env ADVERTISED_PORT=9092 --env KAFKA_LOG4J_ROOT_LOGLEVEL=DEBUG --net 262-network --hostname kafka --ip='172.18.0.22' --name kafka spotify/kafka
+docker run --rm -it -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=kafka --env ADVERTISED_PORT=9092 --env KAFKA_LOG4J_ROOT_LOGLEVEL=DEBUG --net 262-network --hostname kafka --ip='172.18.0.22' --name kafka spotify/kafka
